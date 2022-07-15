01e9d9e082499cd823b7cfc900323b31cad2235e
Hallison Melo <hallison.pm@gmail.com>
2022-07-15 11:43:34 -0300

    constantes

---
+++ utils/constants.js
@@ -0,0 +1,27 @@
+const OPENING_BALANCE = 300;
+const PROPERTIES = [
+  { id: 0, owner: null, saleValue: 0, rentValue:0 },
+  { id: 1, owner: null, saleValue: 0, rentValue:0 },
+  { id: 2, owner: null, saleValue: 0, rentValue:0 },
+  { id: 3, owner: null, saleValue: 0, rentValue:0 },
+  { id: 4, owner: null, saleValue: 0, rentValue:0 },
+  { id: 5, owner: null, saleValue: 0, rentValue:0 },
+  { id: 6, owner: null, saleValue: 0, rentValue:0 },
+  { id: 7, owner: null, saleValue: 0, rentValue:0 },
+  { id: 8, owner: null, saleValue: 0, rentValue:0 },
+  { id: 9, owner: null, saleValue: 0, rentValue:0 },
+  { id: 10, owner: null, saleValue: 0, rentValue:0 },
+  { id: 11, owner: null, saleValue: 0, rentValue:0 },
+  { id: 12, owner: null, saleValue: 0, rentValue:0 },
+  { id: 13, owner: null, saleValue: 0, rentValue:0 },
+  { id: 14, owner: null, saleValue: 0, rentValue:0 },
+  { id: 15, owner: null, saleValue: 0, rentValue:0 },
+  { id: 16, owner: null, saleValue: 0, rentValue:0 },
+  { id: 17, owner: null, saleValue: 0, rentValue:0 },
+  { id: 18, owner: null, saleValue: 0, rentValue:0 },
+  { id: 19, owner: null, saleValue: 0, rentValue:0 },
+];
+module.exports = {
+  OPENING_BALANCE,
+  PROPERTIES,
+};
